549eb7206b51d3e88685c16ef8bfaa59f588cf3f
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2023-10-22 13:43:52 -0400

    add base parser and test class files, and add initial test cases

---
+++ src/test/kotlin/NumberParserTest.kt
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test
+
+class NumberParserTest {
+    @Test
+    fun `Test single character roman numerals are converted to Ints correctly`() {
+
+    }
+
+    @Test
+    fun `Test standard multi character roman numerals are converted to Ints correctly`() {
+
+    }
+
+    @Test
+    fun `Test complicated multi character roman numerals are converted to Ints correctly`() {
+
+    }
+
+    @Test
+    fun `Test Ints are converted to single character roman numerals correctly`() {
+
+    }
+
+    @Test
+    fun `Test Ints are converted to simple multi character roman numerals correctly`() {
+
+    }
+
+    @Test
+    fun `Test Ints are converted to complex multi character roman numerals correctly`() {
+
+    }
+}
