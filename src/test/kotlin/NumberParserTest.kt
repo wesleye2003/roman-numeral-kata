1c04e3d55c22f72302eb3bc77bc4a866541b7b74
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2023-10-22 14:21:23 -0400

    replace assert with AssertJ assertThat for better error messages

--- src/test/kotlin/NumberParserTest.kt
+++ src/test/kotlin/NumberParserTest.kt
@@ -1,33 +1,34 @@
 import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
 class NumberParserTest {
     @Test
     fun `Test single character roman numerals are converted to Ints correctly`() {
-        assert(NumberParser.parseIntFromRomanNumerals("I") == 1)
+        assertThat(NumberParser.parseIntFromRomanNumerals("I")).isEqualTo(1)
     }
 
     @Test
     fun `Test standard multi character roman numerals are converted to Ints correctly`() {
-        assert(NumberParser.parseIntFromRomanNumerals("II") == 2)
+        assertThat(NumberParser.parseIntFromRomanNumerals("II")).isEqualTo(2)
     }
 
     @Test
     fun `Test complicated multi character roman numerals are converted to Ints correctly`() {
-        assert(NumberParser.parseIntFromRomanNumerals("IV") == 4)
+        assertThat(NumberParser.parseIntFromRomanNumerals("IV")).isEqualTo(4)
     }
 
     @Test
     fun `Test Ints are converted to single character roman numerals correctly`() {
-        assert(NumberParser.parseRomanNumeralsFromInt(1) == "I")
+        assertThat(NumberParser.parseRomanNumeralsFromInt(1)).isEqualTo("I")
     }
 
     @Test
     fun `Test Ints are converted to simple multi character roman numerals correctly`() {
-        assert(NumberParser.parseRomanNumeralsFromInt(2) == "II")
+        assertThat(NumberParser.parseRomanNumeralsFromInt(2)).isEqualTo("II")
     }
 
     @Test
     fun `Test Ints are converted to complex multi character roman numerals correctly`() {
-        assert(NumberParser.parseRomanNumeralsFromInt(4) == "IV")
+        assertThat(NumberParser.parseRomanNumeralsFromInt(4)).isEqualTo("IV")
     }
 }
