8a2e31cf984e09d9fddd370f149f28fd2d2d0a44
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2023-10-22 14:05:54 -0400

    add constants for base numerals

--- src/main/kotlin/NumberParser.kt
+++ src/main/kotlin/NumberParser.kt
@@ -1,4 +1,11 @@
 object NumberParser {
+    private const val numeralOne = "I"
+    private const val numeralFive = "V"
+    private const val numeralTen = "X"
+    private const val numeralFifty = "L"
+    private const val numeralOneHundred = "C"
+    private const val numeralFiveHundred = "D"
+    private const val numeralOneThousand = "M"
     fun parseIntFromRomanNumerals(numString: String): Int {
 
     }
