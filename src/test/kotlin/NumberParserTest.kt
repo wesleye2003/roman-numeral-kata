b6b5a88fd58dae253393400bc682c0da9b6cd53f
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2023-10-22 18:04:42 -0400

    add additional test scenarios

--- src/test/kotlin/NumberParserTest.kt
+++ src/test/kotlin/NumberParserTest.kt
@@ -17,11 +17,14 @@ class NumberParserTest {
     @Test
     fun `parseIntFromRomanNumerals converts multi character roman numerals to Ints correctly`() {
         assertThat(NumberParser.parseIntFromRomanNumerals("II")).isEqualTo(2)
+        assertThat(NumberParser.parseIntFromRomanNumerals("II")).isEqualTo(2)
     }
 
     @Test
     fun `parseIntFromRomanNumerals converts complicated multi character roman numerals to Ints correctly`() {
         assertThat(NumberParser.parseIntFromRomanNumerals("IV")).isEqualTo(4)
+        assertThat(NumberParser.parseIntFromRomanNumerals("CMXCIX")).isEqualTo(999)
+        assertThat(NumberParser.parseIntFromRomanNumerals("MMCCCLXXII")).isEqualTo(2372)
     }
 
     @Test
@@ -49,15 +52,60 @@ class NumberParserTest {
         val invalidFifteenError = assertThrows<Error> {
             NumberParser.parseIntFromRomanNumerals("VVV")
         }
+        val invalidOneHundredFiftyError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("LLL")
+        }
+        val invalidFifteenHundredError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("DDD")
+        }
+        val invalidFiveError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("VX")
+        }
+        val invalidNineHundredNinetyFiveError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("VM")
+        }
+        val invalidFiftyError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("LC")
+        }
+        val invalidNineHundredNinetyNineError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("IM")
+        }
+        val invalidFortyNineError = assertThrows<Error> {
+            NumberParser.parseIntFromRomanNumerals("IL")
+        }
 
         assertThat(singleLetterError).isInstanceOf(Error::class.java)
         assertThat(singleLetterError.message).isEqualTo("Invalid Number String")
+
         assertThat(capitalWordError).isInstanceOf(Error::class.java)
         assertThat(capitalWordError.message).isEqualTo("Invalid Number String")
+
         assertThat(lowercaseWordError).isInstanceOf(Error::class.java)
         assertThat(lowercaseWordError.message).isEqualTo("Invalid Number String")
+
         assertThat(invalidFifteenError).isInstanceOf(Error::class.java)
         assertThat(invalidFifteenError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidOneHundredFiftyError).isInstanceOf(Error::class.java)
+        assertThat(invalidOneHundredFiftyError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidFifteenHundredError).isInstanceOf(Error::class.java)
+        assertThat(invalidFifteenHundredError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidFiveError).isInstanceOf(Error::class.java)
+        assertThat(invalidFiveError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidNineHundredNinetyFiveError).isInstanceOf(Error::class.java)
+        assertThat(invalidNineHundredNinetyFiveError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidFiftyError).isInstanceOf(Error::class.java)
+        assertThat(invalidFiftyError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidNineHundredNinetyNineError).isInstanceOf(Error::class.java)
+        assertThat(invalidNineHundredNinetyNineError.message).isEqualTo("Invalid Number String")
+
+        assertThat(invalidFortyNineError).isInstanceOf(Error::class.java)
+        assertThat(invalidFortyNineError.message).isEqualTo("Invalid Number String")
     }
 
     @Test
