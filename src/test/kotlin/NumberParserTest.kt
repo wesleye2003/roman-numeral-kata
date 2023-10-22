961c1937fc46d809cc78e2db07c232b1523446df
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2023-10-22 13:55:31 -0400

    add initial assertions

--- src/test/kotlin/NumberParserTest.kt
+++ src/test/kotlin/NumberParserTest.kt
@@ -3,31 +3,31 @@ import org.junit.jupiter.api.Test
 class NumberParserTest {
     @Test
     fun `Test single character roman numerals are converted to Ints correctly`() {
-
+        assert(NumberParser.parseIntFromRomanNumerals("I") == 1)
     }
 
     @Test
     fun `Test standard multi character roman numerals are converted to Ints correctly`() {
-
+        assert(NumberParser.parseIntFromRomanNumerals("II") == 2)
     }
 
     @Test
     fun `Test complicated multi character roman numerals are converted to Ints correctly`() {
-
+        assert(NumberParser.parseIntFromRomanNumerals("IV") == 4)
     }
 
     @Test
     fun `Test Ints are converted to single character roman numerals correctly`() {
-
+        assert(NumberParser.parseRomanNumeralsFromInt(1) == "I")
     }
 
     @Test
     fun `Test Ints are converted to simple multi character roman numerals correctly`() {
-
+        assert(NumberParser.parseRomanNumeralsFromInt(2) == "II")
     }
 
     @Test
     fun `Test Ints are converted to complex multi character roman numerals correctly`() {
-
+        assert(NumberParser.parseRomanNumeralsFromInt(4) == "IV")
     }
 }
