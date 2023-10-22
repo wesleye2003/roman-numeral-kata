65da63eb94204d7927d9666401127482aa8d7088
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2023-10-22 14:41:28 -0400

    add private simple number conversion function

--- src/main/kotlin/NumberParser.kt
+++ src/main/kotlin/NumberParser.kt
@@ -7,10 +7,27 @@ object NumberParser {
     private const val numeralFiveHundred = "D"
     private const val numeralOneThousand = "M"
     fun parseIntFromRomanNumerals(numString: String): Int {
-        return 0
+        return convertSimpleNumeralToInt(numString)
     }
 
     fun parseRomanNumeralsFromInt(intToConvert: Int): String {
         return "Test"
+    }
+
+    private fun convertSimpleNumeralToInt(numString: String): Int {
+        return when (numString) {
+            numeralOne -> 1
+            numeralFive -> 5
+            numeralTen -> 10
+            numeralFifty -> 50
+            numeralOneHundred -> 100
+            numeralFiveHundred -> 500
+            numeralOneThousand -> 1000
+            else -> throw Error("Invalid Number String")
+        }
+    }
+
+    private fun convertComplexNumeralToInt(numString: String): Int {
+        return 0
     }
 }
